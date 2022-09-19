9ed23f8cae7f0b9efa3903821ae26e7fc1d6b5f6
Honarpishefard <108079784+Honarpishefard@users.noreply.github.com>
2022-09-19 20:07:46 +0430

    Add files via upload

--- Js/DatailsHandler.js
+++ Js/DatailsHandler.js
@@ -9,24 +9,35 @@ const productImage = document.getElementById("productImage");
 const productTittle = document.getElementById("productTittle");
 const ProductPrice = document.getElementById("ProductPrice");
 const addToCard = document.getElementById("addToCard");
+const relatedProductsDiv = document.getElementById("relatedProductsDiv");
 
 
 export default function DatailsPageHandler() {
     let clickedLc = localStorage.getItem("clickedOn");
 
     let chosenProduct = null;
+    let dataLength = null;
+    let productList = null;
     switch (localStorage.getItem("pageId")) {
         case "bathroomProducts":
             chosenProduct = bathroomProductsSource().filter(item => item.id === parseInt(clickedLc))[0];
+            productList = bathroomProductsSource().filter((item) => item.id !== chosenProduct.id);
+            dataLength = bathroomProductsSource().length;
             break;
         case "lightingProducts":
             chosenProduct = lightingProductsSource().filter(item => item.id === parseInt(clickedLc))[0];
+            productList = lightingProductsSource().filter((item) => item.id !== chosenProduct.id);
+            dataLength = lightingProductsSource().length;
             break;
         case "bedroomProducts":
             chosenProduct = bedroomProductsSource().filter(item => item.id === parseInt(clickedLc))[0];
+            productList = bedroomProductsSource().filter((item) => item.id !== chosenProduct.id);
+            dataLength = bedroomProductsSource().length;
             break;
         case "decorativeProducts":
             chosenProduct = decorativeProductsSource().filter(item => item.id === parseInt(clickedLc))[0];
+            productList = decorativeProductsSource().filter((item) => item.id !== chosenProduct.id);
+            dataLength = decorativeProductsSource().length;
             break;
     };
 
@@ -47,7 +58,7 @@ export default function DatailsPageHandler() {
         if (lcCardCheck) {
             const searchIndex = lcCardCheck.findIndex((item) => item.id == chosenProduct.id);
             if (searchIndex !== -1) return alert("This Item is Already Added To Your Card");
-            
+
             const merge = [...lcCardCheck, chosenProduct];
             localStorage.setItem("card", JSON.stringify(merge));
         } else {
@@ -65,4 +76,47 @@ export default function DatailsPageHandler() {
     };
 
     addToCard.addEventListener("click", addToCardFn);
+
+
+    function randomIntFromInterval(min, max) { // min and max included 
+        return Math.floor(Math.random() * (max - min + 1) + min)
+    };
+
+    const rndInt = randomIntFromInterval(productList[0].id, dataLength);
+
+
+    let product1 = null;
+    let product2 = null;
+    let product3 = null;
+    if (rndInt !== dataLength && rndInt !== 1) {
+        product1 = rndInt - 1;
+        product2 = rndInt;
+        product3 = rndInt + 1;
+    } else if (rndInt === dataLength) {
+        product1 = rndInt - 2;
+        product2 = rndInt - 1;
+        product3 = rndInt;
+    } else if (rndInt === productList[0].id) {
+        product1 = rndInt;
+        product2 = rndInt + 1;
+        product3 = rndInt + 2;
+    };
+
+    const relatedProduct1 = productList.filter((item) => item.id === product1);
+    const relatedProduct2 = productList.filter((item) => item.id === product2);
+    const relatedProduct3 = productList.filter((item) => item.id === product3);
+    const relatedProducts = [...relatedProduct1, ...relatedProduct2, ...relatedProduct3];
+
+    relatedProducts.forEach((item) => {
+        const el = `<div style="width: min-content;" id="">
+    <img src="${item.cover}" width="300px" alt="">
+    <p class="mb-0 mt-2 fw-500 fs-18 ps-1">${item.name}</p>
+    <p class="mb-0 mt-1 fw-light fs-6 ps-1">${item.price}</p>
+    </div>`;
+
+        relatedProductsDiv.innerHTML += el;
+    });
+
+
+
 };
--- Js/cardHandler.js
+++ Js/cardHandler.js
@@ -1,12 +1,11 @@
+import discountHandler from "./utils/discountCoupon.js";
+
 const cardContainer = document.querySelector(".card-container");
-const couponInput = document.getElementById("couponInput");
 const applyCouponBttn = document.getElementById("applyCouponBttn");
 const Subtotal = document.getElementById("Subtotal");
 const Shipping = document.getElementById("Shipping");
 const Total = document.getElementById("Total");
-const totalParent = document.getElementById("totalParent");
-const i = document.getElementsByTagName("i")[0];
-
+const checkoutBttn = document.getElementById("checkoutBttn");
 
 
 export default function cardHandler() {
@@ -74,31 +73,11 @@ export default function cardHandler() {
     Total.innerHTML = (sum + Number(Shipping.innerHTML)).toFixed(2);
 
 
-    let discountApplied = false;
-    applyCouponBttn.addEventListener("click", () => {
-        if (discountApplied === true) return alert("Discount is Already Applied");
-        if (couponInput.value === "BLACKFRIDAY" && discountApplied === false) {
-            const discount = ((Total.innerHTML) * .2).toFixed(2);
-            const finalPrice = (Total.innerHTML - discount).toFixed(2);
-            const discountPrice = document.createElement("p");
-            discountPrice.innerHTML = "$" + " " + finalPrice;
-            Total.style.textDecoration = "line-through";
-            i.style.textDecoration = "line-through";
-            i.style.color = "#848484";
-            Total.style.color = "#848484";
+    applyCouponBttn.addEventListener("click", discountHandler);
 
-            const test = document.createElement("p");
-            test.innerHTML = "-20%";
-            test.style.display = "inline-block";
-            test.style.marginLeft = "2rem";
-            test.style.color = "#cb0000";
-            test.style.textDecoration = "none";
-            totalParent.append(test);
 
-            totalParent.appendChild(discountPrice);
-            discountApplied = true;
-        } else {
-            alert("please enter a valid coupon code");
-        };
+    checkoutBttn.addEventListener("click", () => {
+        console.log("ckeck out");
     });
+
 };
