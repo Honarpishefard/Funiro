add63a5b90f3b642c556ccb6d4731413bbac82a8
Honarpishefard <108079784+Honarpishefard@users.noreply.github.com>
2022-10-11 21:07:53 +0330

    Add files via upload

--- Js/index.js
+++ Js/index.js
@@ -2,26 +2,28 @@ import homePageCardsHandler from "./../Js/homePageCardsHandler.js"
 import productListHandler from "./productListHandler.js";
 import DatailsPageHandler from "./DatailsHandler.js";
 import cardHandler from "./cardHandler.js";
+import checkIfLogedIn from "./utils/checkLogedIn.js"
 
+const headerLogIn = document.getElementById("headerLogIn");
 
 let page = document.body.id;
-
-
-
-
-
-
+// const logedStatusLc = localStorage.getItem("logedStatus");
+// const userNameLc = localStorage.getItem("userName");
 
 switch (page) {
     case "homePage":
         homePageCardsHandler();
+        checkIfLogedIn();
         break;
     case "productList":
         productListHandler();
+        checkIfLogedIn();
         break;
     case "productDatails":
         DatailsPageHandler();
+        checkIfLogedIn();
         break;
     case "yourCard":
         cardHandler();
+        checkIfLogedIn();
 };
